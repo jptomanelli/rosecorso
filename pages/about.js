b907c09d3c4c7bf985997e8d41fbf893dc700c64
John Tomanelli II <jptomanelli@gmail.com>
2019-08-31 10:02:24 -0400

    Fix img onload when already loaded

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Page from '../components/page';
 import style from "./index.css";
 import Link from 'next/link';
 
 function About() {
 
+  const img = useRef(null);
   const [loaded, setLoaded] = useState(false);
   const handleOnLoad = () => setLoaded(true);
   
   const links = <Link href='/'><a className={style.a}>R&middot;C</a></Link>;
+
+  useEffect(() => {
+    if (img.current.complete) {
+      return handleOnLoad();
+    }
+  }, []);
 
   return (
     <Page links={links}>
       <div className={style.container}>
         <div className={style.div_split_l}>
           <picture>
-            <img onLoad={handleOnLoad} className={`${style.profile} ${loaded ? undefined : style.opacity_0}`} src="/static/images/7.png" />
+            <img ref={img} onLoad={handleOnLoad} className={`${style.profile} ${loaded ? '': style.opacity_0}`} src="/static/images/7.png" />
           </picture>
         </div>
         <div className={style.div_split_r}>
-          <h1 className={`${style.h1_about} ${loaded ? undefined : style.opacity_0}`}>
+          <h1 className={`${style.h1_about} ${loaded ? '' : style.opacity_0}`}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
